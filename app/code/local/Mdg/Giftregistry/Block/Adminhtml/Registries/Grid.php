1a5cea556ac47e34b166f2c2340bf6143ded77c1
Otto Meier <otto@kesselgulasch.org>
2017-11-23 16:43:12 +0100

    Testing .gitignore

--- app/code/local/Mdg/Giftregistry/Block/Adminhtml/Registries/Grid.php
+++ app/code/local/Mdg/Giftregistry/Block/Adminhtml/Registries/Grid.php
@@ -1,7 +1,9 @@
 <?php
+
 class Mdg_Giftregistry_Block_Adminhtml_Registries_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
         $this->setId('registriesGrid');
         $this->setDefaultSort('event_date');
@@ -9,7 +11,8 @@ class Mdg_Giftregistry_Block_Adminhtml_Registries_Grid extends Mage_Adminhtml_Bl
         $this->setSaveParametersInSession(true);
     }
 
-    protected function _prepareCollection(){
+    protected function _prepareCollection()
+    {
         $collection = Mage::getModel('mdg_giftregistry/entity')->getCollection();
         $this->setCollection($collection);
         return parent::_prepareCollection();
@@ -18,27 +21,27 @@ class Mdg_Giftregistry_Block_Adminhtml_Registries_Grid extends Mage_Adminhtml_Bl
     protected function _prepareColumns()
     {
         $this->addColumn('entity_id', array(
-            'header'   => Mage::helper('mdg_giftregistry')->__('Id'),
-            'width'    => 50,
-            'index'    => 'entity_id',
+            'header' => Mage::helper('mdg_giftregistry')->__('Id'),
+            'width' => 50,
+            'index' => 'entity_id',
             'sortable' => false,
         ));
 
         $this->addColumn('event_location', array(
-            'header'   => Mage::helper('mdg_giftregistry')->__('Location'),
-            'index'    => 'event_location',
+            'header' => Mage::helper('mdg_giftregistry')->__('Location'),
+            'index' => 'event_location',
             'sortable' => false,
         ));
 
         $this->addColumn('event_date', array(
-            'header'   => Mage::helper('mdg_giftregistry')->__('Event Date'),
-            'index'    => 'event_date',
+            'header' => Mage::helper('mdg_giftregistry')->__('Event Date'),
+            'index' => 'event_date',
             'sortable' => false,
         ));
 
         $this->addColumn('type_id', array(
-            'header'   => Mage::helper('mdg_giftregistry')->__('Event Type'),
-            'index'    => 'type_id',
+            'header' => Mage::helper('mdg_giftregistry')->__('Event Type'),
+            'index' => 'type_id',
             'sortable' => false,
         ));
         return parent::_prepareColumns();
@@ -49,6 +52,7 @@ class Mdg_Giftregistry_Block_Adminhtml_Registries_Grid extends Mage_Adminhtml_Bl
         return $this->getUrl('*/*/edit', array('id' => $row->getEntityId()));
     }
 
-    protected function _prepareMassaction(){
+    protected function _prepareMassaction()
+    {
     }
 }
